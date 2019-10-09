0b6fdf3352d72ad3841562e339237483b04f7bc3
Kelly LUO <kelly.luoying@foxmail.com>
2019-10-09 12:46:45 +0200

    Brute force search.

--- kelly.r
+++ kelly.r
@@ -1,3 +1,4 @@
+RNGkind(sample.kind = "Rounding")
 set.seed(42)
 n <- 2000
 knapsack_objects <- data.frame(
@@ -5,7 +6,7 @@ knapsack_objects <- data.frame(
   v=runif(n = n, 0, 10000)
 )
 
-brute_force_knapsack1 = function(x, W){
+brute_force_knapsack = function(x, W){
   stopifnot(all(x > 0) && W > 0)
   num_knapsack <- nrow(x)
   comb_list <- lapply(seq_len(num_knapsack), FUN = function(i) combn(1:num_knapsack, i))
@@ -15,7 +16,7 @@ brute_force_knapsack1 = function(x, W){
   for (i in comb_list){
     for(j in 1:ncol(i)){
       if(sum(x[i[,j],1]) <= W && sum(x[i[,j],2]) > total_value){
-        total_value <- sum(x[i[,j],2])
+        total_value <- round(sum(x[i[,j],2]))
         elements <- i[,j]
       }
     }
@@ -25,33 +26,8 @@ brute_force_knapsack1 = function(x, W){
 }
 
 
-brute_force_knapsack1(x = knapsack_objects[1:8,], W = 3500)
-brute_force_knapsack1(x = knapsack_objects[1:12,], W = 3500)
-brute_force_knapsack1(x = knapsack_objects[1:8,], W = 2000)
-brute_force_knapsack1(x = knapsack_objects[1:12,], W = 2000)
-
-
-brute_force_knapsack = function(x, W){
-  stopifnot(all(x > 0) && W > 0)
-  num_knapsack <- nrow(x)
-  comb_list <- combn(seq_len(num_knapsack), 2)
-  
-  total_value <- 0
-  elements <- vector()
-  for (i in 1:ncol(comb_list)){
-    if(sum(x[comb_list[,i],1]) <= W && sum(x[comb_list[,i],2]) > total_value){
-      total_value <- sum(x[comb_list[,i],2])
-      elements <- comb_list[,i]
-    }
-  }
-  result <- list(value = total_value, elements= elements)
-  return(result)
-}
-
 brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
 brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500)
 brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000)
 brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
 
-
-print(knapsack_objects[1:8,])
