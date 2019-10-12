9bc593a5c231621004116aa009e03593b8b85211
paran0idl <lizu0817@me.com>
2019-10-12 12:16:36 +0200

    try to solve unexpected symbol

--- brute_force_cpp.cpp
+++ brute_force_cpp.cpp
@@ -1,16 +1,8 @@
-#include <Rcpp.h>
+#include <Rcpp.h> 
 #include <vector>
 #include <math.h>
 using namespace Rcpp;
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
+using namespace std;
 
 // [[Rcpp::export]]
 List brute_force(IntegerVector w, NumericVector v ,int W) {
@@ -41,30 +33,3 @@ List brute_force(IntegerVector w, NumericVector v ,int W) {
   
   return L;
 }
-  
-  /*
-  int allCase =  static_cast<int>(pow(2, 5));
-  int maxValue=0;
-  for(int i=0;i<=allCase;i++){
-    int currentCase=i;
-    int currentWeight=0;
-    int currentValue=0;
-    for(int j=0;j<=sizeof(w);j++){
-      if(currentCase&1){
-        currentWeight+=w[j];
-        currentValue+=v[j];
-      }
-      if(currentWeight>W){
-        break;
-      }
-      currentCase=currentCase>>1;
-    }
-    if(currentWeight<=W&&currentValue>maxValue){
-      maxValue=currentValue;
-    }
-  }
-  return maxValue;
-   */
-
-
-
